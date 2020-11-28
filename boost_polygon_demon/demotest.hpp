966611d31f1c2aca419c6659a15a00e35b303ac1
liang.xie <liang.xie>
2020-11-28 16:38:46 +0800

    update code

--- boost_polygon_demon/demotest.hpp
+++ boost_polygon_demon/demotest.hpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <deque>
+
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
+
 #include <boost/foreach.hpp>
-#include <chrono>
-#include <time.h>
 
 
 using namespace std;
@@ -107,6 +108,99 @@ void demoTest4(){
 
     boost::geometry::read_wkt(
         "POLYGON((2 1, 2 2 , 6 2 , 5 1.5, 6 1.5, 6 1 , 2 1))", blue);
+
+    std::deque<polygon> output;
+    int64_t startTime = CurrentTimeMillis();
+    boost::geometry::intersection(green, blue, output);
+
+    int i = 0;
+    std::cout << "green && blue:" << std::endl;
+    BOOST_FOREACH(polygon const& p, output)
+    {
+        std::cout << i++ << ": " << boost::geometry::area(p) << std::endl;
+    }
+    std::cout << "time is: " <<CurrentTimeMillis() - startTime << " ms" << std::endl;
+}
+
+
+void demoTest5(){
+    typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
+
+    polygon green, blue;
+
+    std::string shape1 = "POLYGON((0 0, 0 4, 4 4, 4 0, 0 0))";
+    std::string shape2 = "POLYGON((2 2, 4 0, 3 0.5, 2 0, 2 2))";
+    boost::geometry::read_wkt(shape1, green);
+    boost::geometry::read_wkt(shape2, blue);
+
+    std::deque<polygon> output;
+    int64_t startTime = CurrentTimeMillis();
+    boost::geometry::intersection(green, blue, output);
+
+    int i = 0;
+    std::cout << "green && blue:" << std::endl;
+    BOOST_FOREACH(polygon const& p, output)
+    {
+        std::cout << i++ << ": " << boost::geometry::area(p) << std::endl;
+    }
+    std::cout << "time is: " <<CurrentTimeMillis() - startTime << " ms" << std::endl;
+}
+
+void demoTest6(){
+    typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
+
+    polygon green, blue;
+
+    std::string shape1 = "POLYGON((220 276, 630 274, 4635 269, 4627 46, 242 39, 222 276, 220 276))";
+    std::string shape2 = "POLYGON((221 0, 221 431, 1183 431, 1183 0, 221 0))";
+    boost::geometry::read_wkt(shape1, green);
+    boost::geometry::read_wkt(shape2, blue);
+
+    std::deque<polygon> output;
+    int64_t startTime = CurrentTimeMillis();
+    boost::geometry::intersection(green, blue, output);
+
+    int i = 0;
+    std::cout << "green && blue:" << std::endl;
+    BOOST_FOREACH(polygon const& p, output)
+    {
+        std::cout << i++ << ": " << boost::geometry::area(p) << std::endl;
+    }
+    std::cout << "time is: " <<CurrentTimeMillis() - startTime << " ms" << std::endl;
+}
+
+void demoTest7(){
+    typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
+
+    polygon green, blue;
+
+    std::string shape1 = "POLYGON((221 314, 363 317, 418 389, 622 296, 600 123, 450 77, 489 219, 356 218, 221 314))";
+    std::string shape2 = "POLYGON((297 285, 297 430, 476 430, 476 285, 297 285))";
+    boost::geometry::read_wkt(shape1, green);
+    boost::geometry::read_wkt(shape2, blue);
+
+    std::deque<polygon> output;
+    int64_t startTime = CurrentTimeMillis();
+    boost::geometry::intersection(green, blue, output);
+
+    int i = 0;
+    std::cout << "green && blue:" << std::endl;
+    BOOST_FOREACH(polygon const& p, output)
+    {
+        std::cout << i++ << ": " << boost::geometry::area(p) << std::endl;
+    }
+    std::cout << "time is: " <<CurrentTimeMillis() - startTime << " ms" << std::endl;
+}
+
+void demoTest8(){
+    typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
+
+    polygon green, blue;
+
+    std::string shape1 = "POLYGON((221 314, 363 317, 418 389, 622 296, 600 123, 450 77, 489 219, 356 218, 221 314))";
+    std::string shape2 = "POLYGON((297 285, 297 430, 476 430, 476 285, 297 285))";
+    boost::geometry::read_wkt(shape1, green);
+    boost::geometry::read_wkt(shape2, blue);
 
     std::deque<polygon> output;
     int64_t startTime = CurrentTimeMillis();
